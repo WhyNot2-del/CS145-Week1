87f559210fa7351e2407cbcdf6b47f16fa0f19c8
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-01-21 12:09:39 -0800

    Variables initialized

--- LocationTest.java
+++ LocationTest.java
@@ -1,3 +1,23 @@
+// Names: Brody Weinkauf, Jackson [LastName], Christopher [Lastname]
+// Class: CS 145
+// Date: 1/21/2025 12:05PM
+// Assignment 1
+// Purpose: To take in user input and create a multi-dimensional array
+
+// imports Scanner class
+import java.util.Scanner;
+
 public class LocationTest {
-    
-}
+
+    public static void main(String[] args) {
+
+        // initializes variables
+        Scanner line = new Scanner(System.in);
+        Scanner token = new Scanner(System.in);
+        Integer column = 0;
+        Integer row = 0;
+        String lines = "";
+        int nums = 0;
+
+    }  // ends main method
+}  // ends LocationTest class
