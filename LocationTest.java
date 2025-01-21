4fed155e59737b4cbf408f226f304d7a727991bd
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-01-21 12:18:42 -0800

    Takes in user input multi-dimensional array numbers and places them into the correct locations in the multi-dimensional array

--- LocationTest.java
+++ LocationTest.java
@@ -25,7 +25,20 @@ public class LocationTest {
         System.out.println("Please enter the # of columns");
         column = token.nextInt();
 
+        int largestNum[][] = new int[row][column];
 
-        
+        //  takes in user inputs and adds each number to the correct location in the multi-dimensional array
+        for (int i = 0; i < row; i++) {
+            System.out.printf("%s%d%s%d%s%n", "Please enter the numbers of row", i+1, " each number seperated by a space. ", column, " numbers needed.");
+            lines = line.nextLine();
+            //  seperates each number inputted by a space and puts them into an array
+            String digits[] = lines.split(" ");
+            //  parses the String variables into ints and places them into the correct location in the multi-dimensional array
+            for (int j = 0; j < column; j++) {
+                nums  = Integer.parseInt(digits[j]);
+                largestNum[i][j] = nums;
+            }  // ends nested for loop
+        }  // ends for loop
+
     }  // ends main method
 }  // ends LocationTest class
